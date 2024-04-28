719bb72df10787f9a4bc54e202bdf862df86af58
raihanwebmaster <raihanemon2015@gmail.com>
2024-04-28 17:58:19 +0200

    learning

--- src/create.ts
+++ src/create.ts
@@ -1,6 +1,6 @@
 /** @format */
 
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient, UserRole } from "@prisma/client";
 
 const prisma = new PrismaClient();
 
@@ -13,25 +13,71 @@ const main = async () => {
 //     },
 //   });
 
-const createMany = await prisma.post.createMany({
-  data: [
-    {
-      title: "This is title",
-      content: "This is Content...",
-      author: "Raihan Uddin",
-    },
-    {
-      title: "This is title",
-      content: "This is Content...",
-      author: "Raihan Uddin",
-    },
-    {
-      title: "This is title",
-      content: "This is Content...",
-      author: "Raihan Uddin",
-    },
-  ],
-});
-  console.log(createMany);
+// const createMany = await prisma.post.createMany({
+//   data: [
+//     {
+//       title: "This is title",
+//       content: "This is Content...",
+//       author: "Raihan Uddin",
+//     },
+//     {
+//       title: "This is title",
+//       content: "This is Content...",
+//       author: "Raihan Uddin",
+//     },
+//     {
+//       title: "This is title",
+//       content: "This is Content...",
+//       author: "Raihan Uddin",
+//     },
+//   ],
+// });
+  // console.log(createMany);
+
+     const createUser = await prisma.user.create({
+        data: {
+            username: "user2",
+            email: "user2@ph.com",
+            role: UserRole.user
+        }
+    });
+    console.log(createUser)
+
+    const createProfile = await prisma.profile.create({
+        data: {
+            bio: "this is bio...",
+            userId: 1
+        }
+    })
+
+    const createCategory = await prisma.category.create({
+        data: {
+            name: "software engineering"
+        }
+    })
+
+    const createPost = await prisma.post.create({
+      data: {
+          title: "this is title 5",
+          content: "this is content of the post. 5",
+          authorId: 3,
+          postCategory: {
+              create: [
+                  {
+                      categoryId: 1
+                  },
+                  {
+                      categoryId: 3
+                  },
+                  {
+                      categoryId: 4
+                  }
+              ]
+          }
+      },
+      include: {
+          postCategory: true
+      }
+  })
 };
 main();
